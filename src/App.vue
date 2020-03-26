b9215a1a036c75e962d34ac156ae513fe47342f8
designervoid <newluciocheg@gmail.com>
2020-03-27 00:57:20 +0300

    make search bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
-  <v-app id="inspire">
-    <v-app-bar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      app
-      color="blue darken-3"
-      dark
-    >
-      <v-toolbar-title
-        style="width: 100px"
-        class="ml-0 pl-4"
-      >
-        <span class="hidden-sm-and-down">Pintext</span>
-      </v-toolbar-title>
+<v-app id="inspire">
+  <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+    <v-toolbar-title style="width: 100px" class="ml-0 pl-4">
+      <span class="hidden-sm-and-down">Pintext</span>
+    </v-toolbar-title>
 
-      <v-combobox
-      prepend-inner-icon="mdi-magnify"
-      solo-inverted
-      flat
-      v-model="model"
-      :items="items"
-      :search-input.sync="search"
-      hide-selected
-      class="hidden-sm-and-down"
-      label="Search"
-
-      persistent-hint
-    >
+    <v-combobox prepend-inner-icon="mdi-magnify" solo-inverted flat v-model="model" :items="hints" :search-input.sync="search" hide-selected class="hidden-sm-and-down" label="Search" persistent-hint :menu-props="menuProps" ref="selectField"
+      @change="onChange()">
       <template v-slot:no-data>
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title>
-              No results matching "<strong>{{ search }}</strong>".
+              Press "enter" to search for "<strong>{{ search }}</strong>".
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
@@ -38,102 +19,74 @@
     </v-combobox>
 
     <v-spacer />
-      <v-btn rounded>
-        <span>Главная</span>
-      </v-btn>
-      <v-btn
-        icon
-        large
-      >
-        <v-avatar
-          size="32px"
-          item
-        >
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-            alt="Vuetify"
-          /></v-avatar>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          main container
+    <v-btn rounded>
+      <span>Главная</span>
+    </v-btn>
+    <v-btn icon large>
+      <v-avatar size="32px" item>
+        <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify" />
+      </v-avatar>
+    </v-btn>
+  </v-app-bar>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        {{ model }}
+      </v-row>
+    </v-container>
+  </v-content>
+  <v-btn bottom color="pink" dark fab fixed right @click="dialog = !dialog">
+    <v-icon>mdi-plus</v-icon>
+  </v-btn>
+  <v-dialog v-model="dialog" width="800px">
+    <v-card>
+      <v-card-title class="grey darken-2">
+        Create pintext
+      </v-card-title>
+      <v-container>
+        <v-row class="mx-2">
+          <v-col cols="12">
+            <v-text-field prepend-icon="mdi-text" placeholder="Notes" />
+          </v-col>
         </v-row>
       </v-container>
-    </v-content>
-    <v-btn
-      bottom
-      color="pink"
-      dark
-      fab
-      fixed
-      right
-      @click="dialog = !dialog"
-    >
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="800px"
-    >
-      <v-card>
-        <v-card-title class="grey darken-2">
-          Create pintext
-        </v-card-title>
-        <v-container>
-          <v-row class="mx-2">
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-text"
-                placeholder="Notes"
-              />
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            text
-            color="primary"
-            @click="dialog = false"
-          >Cancel</v-btn>
-          <v-btn
-            text
-            @click="dialog = false"
-          >Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-app>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text color="primary" @click="dialog = false">Cancel</v-btn>
+        <v-btn text @click="dialog = false">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</v-app>
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    menuProps: {
+      closeOnContentClick: true
     },
-    data: () => ({
-      dialog: false,
-      itemsNavigation: [
-        { icon: 'mdi-contacts', text: 'Contacts' }
-      ],
-      items: ['Gaming', 'Programming', 'Vue', 'Vuetify'],
-      model: '',
-      search: null,
-    }),
+    dialog: false,
+    hints: ['Gaming', 'Programming', 'Vue', 'Vuetify'],
+    model: '',
+    search: null,
+  }),
+  methods: {
+    onChange() {
+      this.$nextTick(() => {
+        this.$refs.selectField.isMenuActive = false;
+      });
+    }
   }
+}
 </script>
 
 <style lang="scss">
-  .v-input__slot {
+.v-input__slot {
     top: 15px;
     margin: 0;
-  }
+}
 </style>
